349864a720b41eb7da19c51250335709a855d80d
MestreXit <adriellk0santos@gmail.com>
2026-09-01 16:11:27 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Configuração Firebase MestreXit.ia
+export const firebaseConfig = {
+apiKey:"AIzaSyASZW0rPGb_-xnsWjlFkV0-d3UH6MNVzYo",
+authDomain:"keys-gerador.firebaseapp.com",
+projectId:"keys-gerador",
+storageBucket:"keys-gerador.firebasestorage.app",
+messagingSenderId:"181530577721",
+appId:"1:181530577721:web:0bb38f186b5241c776f6e8",
+measurementId:"G-S0558S0WPZ"
+};;
